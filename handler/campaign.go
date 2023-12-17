6a86a3d033ddbbfe13587c5e0194fadf5178b1a6
KrOxYs <raifandibaiqi@gmail.com>
2023-12-17 13:55:22 +0700

    get campaigns

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,31 @@
 package handler
 
-// tangkap parameter di handler
-// handler ke service
-// service menentukan repository mana yang dipanggil
-// repository : FindAll, FindByUserID
-// db
+import (
+	"bwastart/campaign"
+	"bwastart/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"bwastart/handler"
 	"bwastart/helper"
 	"bwastart/user"
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -27,22 +26,11 @@ func main() {
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
 
-	campaigns, err := campaignRepository.FindByUserID(1)
-	fmt.Println("DEBUG")
-	fmt.Println("DEBUG")
-	fmt.Println("DEBUG")
-	fmt.Println(len(campaigns))
-	for _, campaign := range campaigns {
-		fmt.Println(campaign.Name)
-		if len(campaign.CampaignImages) > 0 {
-			fmt.Println("jumlah gambar")
-			fmt.Println(len(campaign.CampaignImages))
-			fmt.Println(campaign.CampaignImages[0].FileName)
-		}
-	}
-
 	userService := user.NewService(userRepository)
+	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
+
+	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	userHandler := handler.NewUserHandler(userService, authService)
 
@@ -53,6 +41,7 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
 
 	router.Run()
 }
